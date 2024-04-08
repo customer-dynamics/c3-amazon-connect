ec8d982486d2d77a3f707957363fe95cb83b9930
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-04-08 11:48:13 -0600

    refactor: define functions as resources

--- lib/c3-amazon-connect-stack.ts
+++ lib/c3-amazon-connect-stack.ts
@@ -1,16 +1,38 @@
-import * as cdk from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Architecture, Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path = require('path');
 
-export class C3AmazonConnectStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class C3AmazonConnectStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const createPaymentRequestFunction = new Function(this, 'c3CreatePaymentRequest', {
+      description: "Creates a payment request through the C3 API.",
+      architecture: Architecture.ARM_64,
+      timeout: Duration.seconds(10),
+      runtime: Runtime.NODEJS_20_X,
+      handler: 'index.js',
+      code: Code.fromAsset(path.join(__dirname, 'lambda/c3-create-payment-request')),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'C3AmazonConnectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const reportCustomerActivityFunction = new Function(this, 'c3ReportCustomerActivity', {
+      description: "Reports customer payment activity through C3 to the agent.",
+      architecture: Architecture.ARM_64,
+      timeout: Duration.seconds(10),
+      runtime: Runtime.NODEJS_20_X,
+      handler: 'index.js',
+      code: Code.fromAsset(path.join(__dirname, 'lambda/c3-report-customer-activity')),
+    });
+
+    const tokenizeTransactionFunction = new Function(this, 'c3TokenizeTransaction', {
+      description: "Tokenizes customer payment details and submits to C3 for processing.",
+      architecture: Architecture.ARM_64,
+      timeout: Duration.seconds(10),
+      runtime: Runtime.NODEJS_20_X,
+      handler: 'index.js',
+      code: Code.fromAsset(path.join(__dirname, 'lambda/c3-tokenize-transaction')),
+    });
+
   }
 }
