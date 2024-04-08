a34bad03405028a8a39bc0128dc3a1b856ba87a3
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-04-08 11:20:39 -0600

    refactor: set up initial CDK app

---
+++ test/c3-amazon-connect.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as C3AmazonConnect from '../lib/c3-amazon-connect-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/c3-amazon-connect-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new C3AmazonConnect.C3AmazonConnectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
