db77b68968944fc10afbcc4bfff6705331b26d92
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-04-10 12:01:31 -0600

    test: add tests for checking flow content replacements

--- test/c3-amazon-connect.test.ts
+++ test/c3-amazon-connect.test.ts
@@ -1,6 +1,7 @@
 import { App } from 'aws-cdk-lib';
 import { C3AmazonConnectStack } from '../lib/c3-amazon-connect-stack';
 import { Template } from 'aws-cdk-lib/assertions';
+import { readFileSync } from 'fs';
 
 const mockContext = {
 	amazonConnectInstanceArn: 'placeholder',
@@ -34,4 +35,34 @@ test('Lambdas using latest Node', () => {
 	template.hasResourceProperties('AWS::Lambda::Function', {
 		Runtime: 'nodejs20.x',
 	});
+});
+
+test('Flow module content has all replacements', () => {
+	const app = new App({
+		context: mockContext,
+	});
+	const stack = new C3AmazonConnectStack(app, 'MyTestStack');
+	const template = Template.fromStack(stack);
+	const flowModules = template.findResources('AWS::Connect::ContactFlowModule');
+	for (const flowModuleName of Object.keys(flowModules)) {
+		const flowModule = flowModules[flowModuleName];
+		const flowModuleContent = JSON.stringify(flowModule.Properties.Content);
+		expect(flowModuleContent).not.toContain('<');
+		expect(flowModuleContent).not.toContain('>');
+	}
+});
+
+test('Flow content has all replacements', () => {
+	const app = new App({
+		context: mockContext,
+	});
+	const stack = new C3AmazonConnectStack(app, 'MyTestStack');
+	const template = Template.fromStack(stack);
+	const flowModules = template.findResources('AWS::Connect::ContactFlow');
+	for (const flowModuleName of Object.keys(flowModules)) {
+		const flowModule = flowModules[flowModuleName];
+		const flowModuleContent = JSON.stringify(flowModule.Properties.Content);
+		expect(flowModuleContent).not.toContain('<');
+		expect(flowModuleContent).not.toContain('>');
+	}
 });
