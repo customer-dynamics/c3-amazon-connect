581ef7eb91d8404922fd19d8e7aa58ac8ae58532
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-04-09 20:41:20 -0600

    test: write basic Lambda tests

--- test/c3-amazon-connect.test.ts
+++ test/c3-amazon-connect.test.ts
@@ -1,16 +1,37 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as C3AmazonConnect from '../lib/c3-amazon-connect-stack';
+import { App } from 'aws-cdk-lib';
+import { C3AmazonConnectStack } from '../lib/c3-amazon-connect-stack';
+import { Template } from 'aws-cdk-lib/assertions';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/c3-amazon-connect-stack.ts
-test('SQS Queue Created', () => {
-	//   const app = new cdk.App();
-	//     // WHEN
-	//   const stack = new C3AmazonConnect.C3AmazonConnectStack(app, 'MyTestStack');
-	//     // THEN
-	//   const template = Template.fromStack(stack);
-	//   template.hasResourceProperties('AWS::SQS::Queue', {
-	//     VisibilityTimeout: 300
-	//   });
+const mockContext = {
+	amazonConnectInstanceArn: 'placeholder',
+	amazonConnectSecurityKeyId: 'placeholder',
+	amazonConnectSecurityKeyCertificateContent: 'placeholder',
+	c3Env: 'prod',
+	c3ApiKey: 'placeholder',
+	c3VendorId: 'placeholder',
+	logoUrl: 'placeholder',
+	supportPhone: 'placeholder',
+	supportEmail: 'placeholder',
+};
+
+test('Lambdas using arm64', () => {
+	const app = new App({
+		context: mockContext,
+	});
+	const stack = new C3AmazonConnectStack(app, 'MyTestStack');
+	const template = Template.fromStack(stack);
+	template.hasResourceProperties('AWS::Lambda::Function', {
+		Architectures: ['arm64'],
+	});
+});
+
+test('Lambdas using latest Node', () => {
+	const app = new App({
+		context: mockContext,
+	});
+	const stack = new C3AmazonConnectStack(app, 'MyTestStack');
+	const template = Template.fromStack(stack);
+	template.hasResourceProperties('AWS::Lambda::Function', {
+		Runtime: 'nodejs20.x',
+	});
 });
