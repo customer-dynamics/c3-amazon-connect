2cd5823c6b6c9460e39de6cb21e03758810f2b47
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-04-23 15:09:40 -0600

    refactor: update Zift resource class with secrets

--- lib/payment-gateways/zift.ts
+++ lib/payment-gateways/zift.ts
@@ -3,42 +3,78 @@ import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 
 /**
- * Class for creating the necessary resources to facilitate self-service payments collected through DTMF.
+ * Class for creating the necessary resources for tokenizing transactions with Zift.
  */
 export class Zift {
+	private usernameSecret: Secret;
+	private passwordSecret: Secret;
+	private accountIdSecret: Secret;
+
 	/**
-	 * Creates the necessary resources to facilitate self-service payments collected through DTMF.
+	 * Creates the necessary resources to facilitate tokenizing transactions with Zift.
 	 */
 	constructor(
 		private stack: Stack,
-		private policyStatement: PolicyStatement,
+		private tokenizeTransactionPolicy: PolicyStatement,
 	) {
-		this.createSecrets();
+		this.createUsernameSecret();
+		this.createPasswordSecret();
+		this.createAccountIdSecret();
+		this.addSecretsToTokenizeTransactionPolicy();
 	}
 
-	private createSecrets(): void {
-		console.log('Creating Zift secrets...');
-		const usernameSecret = new Secret(this.stack, 'c3ZiftUsername', {
-			secretName: 'ZIFT_USER_NAME',
-			secretStringValue: SecretValue.unsafePlainText('<Username>'),
+	/**
+	 * Creates a secret for the username of your Zift account.
+	 *
+	 * This secret is required to tokenize transactions with Zift.
+	 */
+	private createUsernameSecret(): void {
+		console.log('Creating Zift username secret...');
+		this.usernameSecret = new Secret(this.stack, 'c3ZiftUsername', {
+			secretName: 'ZIFT_USERNAME',
+			secretStringValue: SecretValue.unsafePlainText('<Your Zift username>'),
 			description: 'The username for your Zift account used by C3.',
 		});
+	}
 
-		const passwordSecret = new Secret(this.stack, 'c3ZiftPassword', {
+	/**
+	 * Creates a secret for password for your Zift account.
+	 *
+	 * This secret is required to tokenize transactions with Zift.
+	 */
+	private createPasswordSecret(): void {
+		console.log('Creating Zift password secret...');
+		this.passwordSecret = new Secret(this.stack, 'c3ZiftPassword', {
 			secretName: 'ZIFT_PASSWORD',
-			secretStringValue: SecretValue.unsafePlainText('<Password>'),
+			secretStringValue: SecretValue.unsafePlainText('<Your Zift password>'),
 			description: 'The password for your Zift account used by C3.',
 		});
+	}
 
-		const accountIdSecret = new Secret(this.stack, 'c3ZiftAccountId', {
+	/**
+	 * Creates a secret for the account ID of your Zift account.
+	 *
+	 * This secret is required to tokenize transactions with Zift.
+	 */
+	private createAccountIdSecret(): void {
+		console.log('Creating Zift account ID secret...');
+		this.accountIdSecret = new Secret(this.stack, 'c3ZiftAccountId', {
 			secretName: 'ZIFT_ACCOUNT_ID',
-			secretStringValue: SecretValue.unsafePlainText('<Account ID>'),
+			secretStringValue: SecretValue.unsafePlainText('<Your Zift account ID>'),
 			description: 'The account ID for your Zift account used by C3.',
 		});
-		this.policyStatement.addResources(
-			usernameSecret.secretArn,
-			passwordSecret.secretArn,
-			accountIdSecret.secretArn,
+	}
+
+	/**
+	 * Updates the permissions for the tokenize transaction policy to include getting secret values for Zift secrets.
+	 *
+	 * This is necessary for the Zift secret values to be retrieved by the tokenize transaction function.
+	 */
+	private addSecretsToTokenizeTransactionPolicy(): void {
+		this.tokenizeTransactionPolicy.addResources(
+			this.usernameSecret.secretArn,
+			this.passwordSecret.secretArn,
+			this.accountIdSecret.secretArn,
 		);
 	}
 }
