e4b031167b49aeecce959fb9dd52f49cd20894bb
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-04-24 09:13:05 -0600

    test: add basic tests for testing feature flags

---
+++ test/features/self-service-dtmf.test.ts
@@ -0,0 +1,54 @@
+import { App } from 'aws-cdk-lib';
+import { C3AmazonConnectStack } from '../../lib/c3-amazon-connect-stack';
+import { Template } from 'aws-cdk-lib/assertions';
+import { Context } from '../../lib/models/context';
+import { C3PaymentGateway } from '../../lib/models/enums/c3-payment-gateway';
+import { C3Environment } from '../../lib/models/enums/c3-environment';
+
+const mockContext: Context = {
+	amazonConnect: {
+		instanceArn: 'placeholder',
+		securityKeyId: 'placeholder',
+		securityKeyCertificateContent: 'placeholder',
+	},
+	c3: {
+		env: C3Environment.Prod,
+		apiKey: 'placeholder',
+		vendorId: 'placeholder',
+		paymentGateway: C3PaymentGateway.Zift,
+	},
+	logoUrl: 'placeholder',
+	supportPhone: 'placeholder',
+	supportEmail: 'placeholder',
+	features: {
+		agentInitiatedDTMF: false,
+		agentInitiatedDigital: false,
+		selfServiceDTMF: true,
+	},
+};
+
+const NUMBER_OF_LAMBDAS = 4;
+
+// Verify created resources for agent-initiated DTMF.
+describe('Self-Service DTMF', () => {
+	const app = new App({
+		context: mockContext,
+	});
+	const stack = new C3AmazonConnectStack(app, 'MyTestStack');
+	const template = Template.fromStack(stack);
+
+	describe('Amazon Connect', () => {
+		describe('DTMF flow module', () => {
+			it('Has contact flow module', () => {
+				template.resourceCountIs('AWS::Connect::ContactFlowModule', 1);
+			});
+		});
+	});
+
+	// Lambda functions
+	describe('Lambda functions', () => {
+		it('Has 4 created functions', () => {
+			template.resourceCountIs('AWS::Lambda::Function', NUMBER_OF_LAMBDAS);
+		});
+	});
+});
