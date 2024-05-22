8cdea245f80b1c931eab7cbdc7c720bb9128d8e7
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-05-22 09:40:52 -0600

    refactor: update email receipt Lambda to only require one secret

--- lib/c3-amazon-connect-stack.ts
+++ lib/c3-amazon-connect-stack.ts
@@ -331,16 +331,11 @@ export class C3AmazonConnectStack extends Stack {
 			codeSigningConfig: this.codeSigningConfig,
 		});
 
-		// Create the policies for getting secret values.
-		const batchGetSecretsPolicy = new PolicyStatement({
-			actions: ['secretsmanager:BatchGetSecretValue'],
-			resources: ['*'],
-		});
+		// Create the policy for getting secret values.
 		const getSecretValuePolicy = new PolicyStatement({
 			actions: ['secretsmanager:GetSecretValue'],
 			resources: [this.c3ApiKeySecret.secretArn],
 		});
-		this.emailReceiptFunction.addToRolePolicy(batchGetSecretsPolicy);
 		this.emailReceiptFunction.addToRolePolicy(getSecretValuePolicy);
 	}
 
