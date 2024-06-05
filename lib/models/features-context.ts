14b51362314d9be4e2d0751b4b6481fea79532a3
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-06-05 13:56:30 -0600

    chore: add option for subject lookup

--- lib/models/features-context.ts
+++ lib/models/features-context.ts
@@ -1,5 +1,34 @@
+import { SubjectLookupMode } from './enums/subject-lookup-mode';
+
 export interface FeaturesContext {
 	agentAssistedIVR: boolean;
 	agentAssistedLink: boolean;
 	selfServiceIVR: boolean;
+	subjectLookup: SubjectLookupMode;
+}
+
+/**
+ * Validates the feature context variables. Throws an error if any of the required variables are missing.
+ *
+ * @param featuresContext The feature context variables.
+ */
+export function validateFeaturesContext(
+	featuresContext: FeaturesContext,
+): void {
+	if (!featuresContext) {
+		throw new Error('Feature context variables are required.');
+	} else if (
+		!Object.values(SubjectLookupMode).includes(featuresContext.subjectLookup)
+	) {
+		throw new Error(
+			`features.subjectLookup context variable is invalid. Must be one of: ${Object.values(SubjectLookupMode).join(', ')}`,
+		);
+	} else if (
+		featuresContext.subjectLookup &&
+		!(featuresContext.agentAssistedIVR || featuresContext.agentAssistedLink)
+	) {
+		throw new Error(
+			'features.subjectLookup requires features.agentAssistedIVR or features.agentAssistedLink to be enabled.',
+		);
+	}
 }
