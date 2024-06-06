e94d6175c96648a3c1dd3c655d837a66c1cc5c98
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2024-06-06 15:05:05 -0600

    test: update number of expected policies to match agent assist

--- test/features/subject-lookup.test.ts
+++ test/features/subject-lookup.test.ts
@@ -94,9 +94,9 @@ describe('Subject Lookup', () => {
 		it('Has 1 created role', () => {
 			template.resourceCountIs('AWS::IAM::Role', NUMBER_OF_LAMBDAS + 1);
 		});
-		it('Has 6 created policies', () => {
-			// Cross org policy, 4 secrets policies, and kms policy
-			template.resourceCountIs('AWS::IAM::Policy', 6);
+		it('Has 5 created policies', () => {
+			// Cross org policy, 3 secrets policies, and kms policy
+			template.resourceCountIs('AWS::IAM::Policy', 5);
 		});
 	});
 });
